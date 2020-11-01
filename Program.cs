aa0fca557b24c666b0cca0eb4eeb7f89bf171584
Ruthikreddy <ruthikreddy15@gmail.com>
2020-11-01 21:31:43 +0530

    [Ruthik] push for UC1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CabInvioceGenerater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Cab Invoice Generator");
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.PREMIUM);
+            double fare = invoiceGenerator.CalculateFare(2.0, 5);
+            Console.WriteLine($"Fare : {fare}");
+        }
+    }
+}
